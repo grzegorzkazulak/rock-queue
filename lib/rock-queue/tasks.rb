3ea05d0f6df2003fd08359ba2cb251dace62bffc
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-11-15 20:59:34 +0100

    rake tasks

---
+++ lib/rock-queue/tasks.rb
@@ -0,0 +1,9 @@
+namespace :rock_queue do
+  desc "Start a Rock Queue worker"
+  task :work do
+    worker = RockQueue::Worker.new
+    worker.verbose = ENV['VERBOSE']
+    puts "*** Starting rock-queue worker #{worker}"
+    worker.work
+  end
+end
