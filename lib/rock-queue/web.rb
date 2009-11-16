3e24f1adfb092dfb70cd5e450c801933ade191ca
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-11-16 23:47:30 +0100

    more work around the web interface

--- lib/rock-queue/web.rb
+++ lib/rock-queue/web.rb
@@ -4,18 +4,21 @@ require 'erb'
 
 module RockQueue
   class Web < Sinatra::Base
-    current_dir = File.dirname(File.expand_path(__FILE__))
     
-    set :views,  "#{current_dir}/web/views"
-    set :public, "#{current_dir}/web/public"
-    set :static, true
-    
-    get "/" do
-      redirect url(:dashbaord)
+    configure do
+      current_dir = File.dirname(File.expand_path(__FILE__))
+      set :views,  "#{current_dir}/web/views"
+      set :public, "#{current_dir}/web/public"
+      set :static, true
+      set :raise_errors, true
     end
     
-    get "/dashboard" do
-      show 'dashboard'
+    get '/' do
+      redirect '/dashboard'
+    end
+    
+    get '/dashboard' do
+      erb :dashboard
     end
     
   end
