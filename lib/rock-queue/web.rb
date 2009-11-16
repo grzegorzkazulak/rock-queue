6b9523972c1e2ac053534c087eca6b62c2aaac9d
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-11-16 23:23:29 +0100

    skeleton for web interface

---
+++ lib/rock-queue/web.rb
@@ -0,0 +1,21 @@
+require 'sinatra/base'
+require 'erb'
+
+module Resque
+  class Web < Sinatra::Base
+    current_dir = File.dirname(File.expand_path(__FILE__))
+    
+    set :views,  "#{current_dir}/web/views"
+    set :public, "#{current_dir}/web/public"
+    set :static, true
+    
+    get "/" do
+      redirect url(:dashbaord)
+    end
+    
+    get "/dashboard" do
+      show 'dashboard'
+    end
+    
+  end
+end
