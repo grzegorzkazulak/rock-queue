5859b8810b9b188f0d6af769c604694e57fe81d9
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-11-17 12:20:53 +0100

    notifier observer

--- lib/rock-queue/notifiers.rb
+++ lib/rock-queue/notifiers.rb
@@ -6,8 +6,20 @@ module RockQueue
       @__instance__ ||= new
     end
     
+    # Return registered notifiers
+    def self.available
+      @@_notifiers ||= []
+      @@_notifiers
+    end
+    
     def register(instance)
-      
+      available << instance
+    end
+    
+    def notify(message)
+      available.each do |notifier|
+        notifier.notify(message)
+      end
     end
   end
 end
