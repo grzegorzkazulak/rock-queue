c8a72da0070084fb910450d5c7d68514731a92da
Daniel Chrusciak <daniel.chrusciak@dnc.pl>
2009-11-17 17:18:17 -0800

    Move QueueObject class to RockQueue module

--- lib/rock-queue/notifiers/email_notifier.rb
+++ lib/rock-queue/notifiers/email_notifier.rb
@@ -6,6 +6,10 @@ module RockQueue
     
     def update(message)
       puts "Sending e-mail message: #{message}"
+      Net::SMTP.start('localhost') do |smtp|
+        smtp.send_message msg, from, to
+      end
     end
+
   end
 end
