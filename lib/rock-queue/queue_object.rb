2c21a2198d00abfe86e7611d6be811d3562e844c
Daniel Chrusciak <daniel.chrusciak@dnc.pl>
2009-11-18 09:18:17 +0800

    Move QueueObject class to RockQueue module

--- lib/rock-queue/queue_object.rb
+++ lib/rock-queue/queue_object.rb
@@ -1,29 +1,33 @@
-# Queue object wrapper
-class QueueObject
-  
-  DEFAULT_FAIL_LIMIT = 3
-  
-  attr_reader :object, :fails
-  
-  def initialize(object)
-    @object = object
-    @fails = Array.new
-    config = RockQueue::Config.settings
-  end
+module RockQueue
 
-  # Add processing fail
-  def add_fail(exception)
-    @fails << exception
-    if @fails.length < DEFAULT_FAIL_LIMIT
-      return true
-    else
-      RockQueue::Notifiers.instance.notify(exception)
+  # Queue object wrapper
+  class QueueObject
+    
+    DEFAULT_FAIL_LIMIT = 3
+    
+    attr_reader :object, :fails
+    
+    def initialize(object)
+      @object = object
+      @fails = Array.new
+      config = RockQueue::Config.settings
     end
+  
+    # Add processing fail
+    def add_fail(exception)
+      @fails << exception
+      if @fails.length < DEFAULT_FAIL_LIMIT
+        return true
+      else
+        RockQueue::Notifiers.instance.notify(exception)
+      end
+    end
+  
+    # Get sleep time after fail
+    def get_sleep_time
+      2 ** @fails.length
+    end
+    
   end
 
-  # Get sleep time after fail
-  def get_sleep_time
-    2 ** @fails.length
-  end
-  
 end
