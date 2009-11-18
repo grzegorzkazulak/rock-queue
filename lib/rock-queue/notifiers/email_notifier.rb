df01ff264f68d5b5e17a8250129b9f10f046350e
Daniel Chrusciak <daniel.chrusciak@dnc.pl>
2009-11-18 10:26:51 -0800

    Email notifier

--- lib/rock-queue/notifiers/email_notifier.rb
+++ lib/rock-queue/notifiers/email_notifier.rb
@@ -1,13 +1,28 @@
+require 'mail'
+
 module RockQueue
+  
   class EmailNotifier < AbstractNotifier
+  
     def initialize(config)
-    
+      @config = config
     end
     
+    # Notify by email  
     def update(message)
       puts "Sending e-mail message: #{message}"
-      Net::SMTP.start('localhost') do |smtp|
-        smtp.send_message msg, from, to
+
+      Mail.defaults do
+        smtp @config[:server], @config[:port]
+        user @config[:username]
+        pass @config[:password]
+      end
+
+      Mail.deliver do
+          from @config[:from]
+            to @config[:to]
+       subject 'Processing error'
+          body message
       end
     end
 
