6ff5273b7acbb8eb2e6371e723e01ac38ce1e01c
Daniel Chrusciak <daniel.chrusciak@dnc.pl>
2009-11-18 09:14:36 +0800

    Retrying message logging patch

--- lib/rock-queue/worker.rb
+++ lib/rock-queue/worker.rb
@@ -26,8 +26,8 @@ module RockQueue
             rescue Object => e
               # Add failed processing and retry
               if queue.add_fail(e)
-                puts "=> Processing fail! Retrying #{queue.fails.length}"
                 sleep(queue.get_sleep_time)
+                puts "=> Processing fail! Retrying #{queue.fails.length}"
                 retry
               end
             end
